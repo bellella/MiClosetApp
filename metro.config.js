8685305d30f9f4e8d1b325317e355b50b79a3e7a
Mina <bellella13@gmail.com>
2026-01-16 16:11:07 +0900

    prebuild test

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,12 @@ const { withStorybook } = require('@storybook/react-native/metro/withStorybook')
 
 let config = getDefaultConfig(__dirname);
 
+config.resolver = {
+    ...config.resolver,
+    // Prioritize CommonJS modules over ESM, bypassing `import.meta` checks
+    unstable_conditionNames: ['browser', 'require', 'react-native'],
+}
+
 config = withStorybook(config);
 
 config = withNativeWind(config, { input: './global.css' });
